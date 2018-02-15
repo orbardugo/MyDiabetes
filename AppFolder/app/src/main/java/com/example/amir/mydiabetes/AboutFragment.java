3a28bd51b60b0b6cd7bc6ba8a572604ca71f8c4e
erezam <amir4246@gmail.com>
2018-02-15 14:14:11 +0200

    About fragment

---
+++ AppFolder/app/src/main/java/com/example/amir/mydiabetes/AboutFragment.java
@@ -0,0 +1,90 @@
+package com.example.amir.mydiabetes;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AboutFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class AboutFragment extends Fragment {
+
+    View view;
+    TextView txtAbout;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_about, container, false);
+        txtAbout = view.findViewById(R.id.txtAbout);
+        String str = "About: \n" +
+                "The application is intended for diabetics to record blood sugar measurements.\n" +
+                "Contains the following options:\n" +
+                "1. Recording glucose (sugar, carbohydrate, insulin).\n" +
+                "2. Measure log.\n" +
+                "3. Testing the average blood glucose for different periods.\n" +
+                "4. Send emergency messages if necessary containing location.\n" +
+                "5. Alerts for measuring sugar.\n" +
+                "\n" +
+                "\n" +
+                "The application was built as part of an Android development course at Azrieli College, by Amir Erez and Or Bardugo.";
+        txtAbout.setText(str);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- AppFolder/app/src/main/java/com/example/amir/mydiabetes/SubmitFragment.java
+++ AppFolder/app/src/main/java/com/example/amir/mydiabetes/SubmitFragment.java
@@ -1,8 +1,10 @@
 package com.example.amir.mydiabetes;
 
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
@@ -32,7 +34,7 @@ import com.google.android.gms.location.LocationServices;
 public class SubmitFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener, View.OnClickListener {
 
-    TextView result;
+    TextView result,headtxt;
     Button smsBtn;
     View view;
     SmsManager smsManager;
@@ -49,12 +51,14 @@ public class SubmitFragment extends Fragment implements GoogleApiClient.Connecti
     }
 
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_submit, container, false);
         result = view.findViewById(R.id.sugarLvlTxt);
+        headtxt = view.findViewById(R.id.txtHead);
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(view.getContext())
                     .addConnectionCallbacks(this)
@@ -72,22 +76,39 @@ public class SubmitFragment extends Fragment implements GoogleApiClient.Connecti
 
         int gluc = getArguments().getInt("glucose");
         String res = null;
-        if (gluc <= 70)
+        // low sugar
+        if (gluc <= 70) {
+            headtxt.setTextColor(Color.parseColor("#FF0000"));
+            headtxt.setText("LOW SUGAR LEVEL");
             res = "Your Blood Sugar Level " + gluc + " is low!\n You should eat or drink something to raise it. \n" +
                     "We'll recommend one of the following:\n" +
                     "* 3 – 4 glucose tablets or gels\n" +
                     "* 4 ounces of fruit juice\n" +
                     "* 6 ounces of regular soda\n" +
                     "* 1 tablespoon of honey\n" +
-                    "* 1 tablespoon of table sugar";
-        else if (gluc >= 250) {
+                    "* 1 tablespoon of table sugar"+
+                    "\n" +
+                    "\n" +
+                    "Click down to send emergency sms with your location:";
+            smsBtn.setVisibility(View.VISIBLE);
+        }
+        else if (gluc >= 250) { //high sugar
+            headtxt.setTextColor(Color.parseColor("#FF0000"));
+            headtxt.setText("HIGH SUGAR LEVEL");
             res = "Your Blood Sugar Level " + gluc + " is high!\n" +
                     "We'll recommend to take insulin injection and  Drink lots of water\n" +
-                    "If your glucose level does not go down, contact your doctor and consider a hospital evacuation.";
+                    "If your glucose level does not go down, contact your doctor and consider a hospital evacuation." +
+                    "\n" +
+                    "\n" +
+                    "Click down to send emergency sms with your location:";
             smsBtn.setVisibility(View.VISIBLE);
 
-        } else
+        }
+        else {  // fine sugar
+            headtxt.setTextColor(Color.parseColor("#7CFC00"));
+            headtxt.setText("SUGAR LEVEL IS GOOD");
             res = "Your Blood Sugar Level " + gluc + " is good!";
+        }
         result.setText(res);
         return view;
     }
